cdb031d794ba493b389904c120a5c26e83dc9f55
bilgekosee <bilge.koosee@gmail.com>
2025-03-31 20:56:31 +0300

    template engine added

--- app.js
+++ app.js
@@ -6,6 +6,12 @@ app.use(express.static("public"));
 app.get("/", (req, res) => {
   res.sendFile(path.resolve(__dirname, "site/index.html"));
 });
+app.get("/about", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "site/about.html"));
+});
+app.get("/blog", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "site/blog.html"));
+});
 const port = 3000;
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
