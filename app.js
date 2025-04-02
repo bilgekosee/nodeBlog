2accdd9563198d6fa1fd785d3463c44dcacfa496
bilgekosee <bilge.koosee@gmail.com>
2025-04-02 22:10:10 +0300

    post added and image upload added

--- app.js
+++ app.js
@@ -2,12 +2,14 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const { engine } = require("express-handlebars");
+const fileUpload = require("express-fileupload");
 const app = express();
 
 mongoose
   .connect("mongodb://127.0.0.1:27017/nodeblog_db")
   .then(() => console.log("Connected!"));
 
+app.use(fileUpload());
 app.use(express.static("public"));
 
 app.engine("handlebars", engine());
--- routes/posts.js
+++ routes/posts.js
@@ -1,12 +1,28 @@
 const express = require("express");
 const router = express.Router();
 const Post = require("../models/Post");
+const path = require("path");
 
 router.get("/new", (req, res) => {
   res.render("site/addpost");
 });
+router.get("/:id", (req, res) => {
+  Post.findById(req.params.id)
+    .lean()
+    .then((post) => {
+      res.render("site/post", { post: post });
+    });
+});
 router.post("/test", (req, res) => {
-  Post.create(req.body);
+  let post_image = req.files.post_image;
+  post_image.mv(
+    path.resolve(__dirname, "../public/img/postImages", post_image.name)
+  );
+  Post.create({
+    ...req.body,
+    post_image: `/public/img/postImages/${post_image.name}`,
+  });
+
   res.redirect("/");
 });
 module.exports = router;
