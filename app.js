33f33d8a8e314f08351dcceb4f76f7cfbabdd692
bilgekosee <bilge.koosee@gmail.com>
2025-04-05 21:26:51 +0300

    connect mongo added

--- app.js
+++ app.js
@@ -2,14 +2,26 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const { engine } = require("express-handlebars");
+const app = express();
 const fileUpload = require("express-fileupload");
 const generateDate = require("./helpers/generateDate").generateDate;
-const app = express();
+const expressSession = require("express-session");
+const MongoStore = require("connect-mongo");
 
 mongoose
   .connect("mongodb://127.0.0.1:27017/nodeblog_db")
   .then(() => console.log("Connected!"));
 
+app.use(
+  expressSession({
+    secret: "testbillG",
+    resave: false,
+    saveUninitialized: true,
+    store: MongoStore.create({
+      mongoUrl: "mongodb://127.0.0.1:27017/nodeblog_db",
+    }),
+  })
+);
 app.use(fileUpload());
 app.use(express.static("public"));
 
@@ -25,6 +37,7 @@ app.use(bodyParser.json());
 const main = require("./routes/main");
 const posts = require("./routes/posts");
 const users = require("./routes/users");
+
 app.use("/", main);
 app.use("/posts", posts);
 app.use("/users", users);
--- routes/users.js
+++ routes/users.js
@@ -26,7 +26,7 @@ router.post("/login", async (req, res) => {
 
     if (user) {
       if (user.password === password) {
-        //user session
+        req.session.userId = user._id;
         res.redirect("/");
       } else {
         res.redirect("/users/login");
