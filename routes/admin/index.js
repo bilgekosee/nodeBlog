40dec406648fa31d5e10125dc0b626e9875ebd5b
bilgekosee <bilge.koosee@gmail.com>
2025-04-06 21:13:34 +0300

    category ekleme

--- routes/admin/index.js
+++ routes/admin/index.js
@@ -1,11 +1,23 @@
 const express = require("express");
 const router = express.Router();
+const Category = require("../../models/Category");
 
 router.get("/", (req, res) => {
   res.render("admin/index");
 });
-router.get("/categories", (req, res) => {
-  res.render("admin/categories");
+router.get("/categories", async (req, res) => {
+  const categories = await Category.find({}).lean();
+  res.render("admin/categories", { categories: categories });
+});
+
+router.post("/categories", async (req, res) => {
+  try {
+    const category = await Category.create(req.body);
+    res.redirect("categories");
+  } catch (error) {
+    console.error("Kategori oluşturulurken hata:", error);
+    res.status(500).send("Bir hata oluştu.");
+  }
 });
 
 module.exports = router;
