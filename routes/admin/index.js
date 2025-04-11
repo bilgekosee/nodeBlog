e9d12e20c9d45aa36e90487ed5beb39ead1bf13e
bilgekosee <bilge.koosee@gmail.com>
2025-04-11 23:09:21 +0300

    admin alanaı post kategori ilişkisi

--- routes/admin/index.js
+++ routes/admin/index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const Category = require("../../models/Category");
+const Post = require("../../models/Post");
+const User = require("../../models/User");
 
 router.get("/", (req, res) => {
   res.render("admin/index");
@@ -24,6 +26,16 @@ router.delete("/categories/:id", async (req, res) => {
   await Category.findByIdAndDelete({ _id: req.params.id }).then(() => {
     res.redirect("/admin/categories");
   });
+});
+
+router.get("/posts", async (req, res) => {
+  Post.find({})
+    .populate({ path: "category", model: Category })
+    .sort({ $natural: -1 })
+    .lean()
+    .then((posts) => {
+      res.render("admin/posts", { posts: posts });
+    });
 });
 
 module.exports = router;
